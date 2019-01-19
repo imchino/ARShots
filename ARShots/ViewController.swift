6bf3cc532020e3ddfc8e12684d01f2c44db8a0e0
chino <chino618@me.com>
2019-01-20 01:03:18 +0900

    光源の自動処理を実装

--- ARShots/ViewController.swift
+++ ARShots/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
